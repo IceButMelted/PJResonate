b670dccf131ac196f3b8aa45238fbdb6224f716f
Chatdanai Kantawong <ice.pu2014@gmail.com>
2023-03-10 01:36:28 +0700

    translation scene prototype complete
    -just add translation form scene to scene

--- Assets/Scenes/Start-memu/Loadnext_easy_ver.cs
+++ Assets/Scenes/Start-memu/Loadnext_easy_ver.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Loadnext_easy_ver : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    public Animator transiton;
+
+    public void LoadNextLevel(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(Transition(sceneName));
     }
+
+    IEnumerator Transition(string sceneName) 
+    {
+        transiton.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(sceneName);
+    } 
 }
